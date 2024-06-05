f8202cfb1f1e93d9c0d8c635773b1cee40eef94c
Gideon Mutai <giddymutai13@gmail.com>
2024-06-05 20:13:29 +0300

    Finished task

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,28 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    # initialize the attributes
+    def __init__(self, title, page_count):
+        self.title = title
+        self.page_count = page_count
+    
+    # methods to set the page_count property
+    @property
+    def page_count(self):
+        """Page count property"""
+        return self._page_count
+    
+    @page_count.setter
+    def page_count(self, page_count):
+        if type(page_count) == int:
+            self._page_count = page_count
+        else:
+            print("page_count must be an integer")
+
+    # method to turn the page
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+    
+
     
         
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,30 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    
+    # initializes the attributes
+    def __init__(self, brand, size):
+        self.brand = brand
+        self.size = size
+        self.condition = self.cobble()
+
+    # size getter and setter methods
+    @property
+    def size(self):
+        """size property"""
+        return self._size
+    
+    @size.setter
+    def size(self, size):
+        if type(size) == int:
+            self._size = size
+        else:
+            print("size must be an integer")
+
+    # method to cobble shoe
+    def cobble(self):
+        # this method when called sets the instance attribute to new
+        self.condition = "New"
+        print("Your shoe is as good as new!")
+        
+        
